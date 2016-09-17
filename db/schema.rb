5a850c8726483418c921e90b4c710cc5bdda798c
akash <akash.s@amuratech.com>
2016-09-17 15:37:31 +0530

    Ran db migrate for admin model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917100308) do
+ActiveRecord::Schema.define(version: 20160917100542) do
+
+  create_table "admins", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "admins", ["user_id"], name: "index_admins_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,4 +39,5 @@ ActiveRecord::Schema.define(version: 20160917100308) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "admins", "users"
 end
