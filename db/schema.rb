34e811a552f38bae885b307b8534eb56096bee55
akash <akash.s@amuratech.com>
2016-09-17 15:58:22 +0530

    ran migrations - akash

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20160917102005) do
 
+  create_table "admins", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "admins", ["user_id"], name: "index_admins_on_user_id", using: :btree
+
   create_table "candidates", force: :cascade do |t|
     t.string   "first_name",     limit: 20,                   null: false
     t.string   "last_name",      limit: 25,                   null: false
@@ -50,4 +58,23 @@ ActiveRecord::Schema.define(version: 20160917102005) do
     t.datetime "updated_at",               null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  add_foreign_key "admins", "users"
 end
