83eebd1e5aa29cac120b4ec4c63d268aa415818b
akash <akash.s@amuratech.com>
2016-09-17 16:01:22 +0530

    generated achievement model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917102005) do
+ActiveRecord::Schema.define(version: 20160917103016) do
+
+  create_table "achievements", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
