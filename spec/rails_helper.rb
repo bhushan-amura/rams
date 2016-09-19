3a403afdb6727b4c42b540900b450c2cd12c5f22
bhushan-amura <bhushan.a@amuratech.com>
2016-09-19 16:00:17 +0530

    Update spec rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-<<<<<<< HEAD
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-=======
 require 'shoulda-matchers'
 
 require "bundler/setup"
@@ -29,7 +27,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 
->>>>>>> develop
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
