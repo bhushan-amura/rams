e210dacc2fbd018c24661bf3b1f265090a4e81b5
bhushan-amura <bhushan.a@amuratech.com>
2016-09-19 17:53:51 +0530

    Updated models events, company, job opening, location

--- app/models/job_opening.rb
+++ app/models/job_opening.rb
@@ -1,4 +1,15 @@
 class JobOpening < ActiveRecord::Base
+
+  include ForbiddenValues
+
   belongs_to :company
   has_many :events
+
+  validates :title, presence:true
+  validates :shift, presence:true
+  validates :description, presence:true
+  validates :number_of_positions, presence:true 
+
+  # after_create : ask_for_candidates_from_agency
+
 end
