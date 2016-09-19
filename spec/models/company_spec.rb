abb0adfbc0a8dc388d866a07cd0b6cffc88a2184
bhushan-amura <bhushan.a@amuratech.com>
2016-09-19 15:31:53 +0530

    Added spec files

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have relation of has many job openings"
+  
+  it "should have relation of has many locations"
 end
