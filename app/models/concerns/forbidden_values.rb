42f19e7733fc7fa798015c52b5d101dd3c55dd5b
jayesh <jayesh@amuratech.com>
2016-09-19 16:42:38 +0530

    updating candidate validation

---
+++ app/models/concerns/forbidden_values.rb
@@ -0,0 +1,12 @@
+module ForbiddenValues
+	extend ActiveSupport::Concern
+	REGEX_NAME_FORMAT = /\A[a-zA-Z]+\Z/
+	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+	FORBIDDEN_EMAILS = /\A[a-z0-9._%+-]+@rams+\.[a-z]{2,4}\Z/i
+
+	def email_is_allowed
+		if email.match(/\A[a-z0-9._%+-]+@rams+\.[a-z]{2,4}\Z/i)
+			errors.add(:email,"error! email has been restricted!")
+		end		
+	end
+end
