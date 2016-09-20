08d1573d1f1fef9924810740039cf79a1d1219d6
bhushan-amura <bhushan.a@amuratech.com>
2016-09-20 12:03:51 +0530

    Updated company validations

--- app/models/company/company.rb
+++ app/models/company/company.rb
@@ -1,17 +1,21 @@
 class Company < ActiveRecord::Base
   include ForbiddenValues
+  include DatabaseStorageFormat
+
+  before_save :lower_fields
 
   has_many :job_openings
   has_many :locations, as: :locatable
+  has_many :reviews
 
-  validates :name, presence:true
-  validates :company_type, presence:true
+  validates :name, presence:true, length: { :maximum => 255 }
+  validates :company_type, presence:true, length: { :maximum => 255 }
   validates :email, presence:true, :length => { :maximum => 100 },
     :format => EMAIL_REGEX, :confirmation => true,
     :uniqueness => true
-  validates :description, presence:true 
-  validates :url, presence:true
-  
-  validate :email_is_allowed
+  validates :description, presence:true
+  validates :url, presence:true, length: { :maximum => 255 }
+
+  validate :vaid_email?
 
 end
