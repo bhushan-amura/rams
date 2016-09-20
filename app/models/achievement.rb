7bab34cbc9db7d643d3901477303aae2d945f791
jayesh <jayesh@amuratech.com>
2016-09-20 20:00:00 +0530

    updated database storage file model concern

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,12 +2,13 @@ class Achievement < ActiveRecord::Base
   
   include DatabaseStorageFormat
   
-  before_save :lower_fields
+  	before_save :lower_fields
 
 	belongs_to :candidate
 
 	validates :title, length: { maximum: 255 }
 	validates :description, length: { maximum: 65535 }
 
-  validates_associated :candidate
+  	validates_associated :candidate
+
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-	 it { should belong_to(:candidate) }
+	
+		let(:candidate) {Candidate.new(first_name:"abcd",last_name:"efgh",dob:'1992/03/15',gender:'M',email:'jayesh@gmail.com',marital_status:"married",status:true,languages:"HIBRU",summary:"dont know",phone:"12321442415",interests:"dddd")}
+
+		
+
+		context "model with attributes valid?" do	
+			it "is valid with valid attributes" do
+			    expect(candidate).to be_valid
+			end
+		end
+	
+
+
 end
+
