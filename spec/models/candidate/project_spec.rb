47a0c417fa5c2cf928fd46ce213a6496f78408a7
jayesh <jayesh@amuratech.com>
2016-09-21 16:32:51 +0530

    created candidate project validation specs

--- spec/models/candidate/project_spec.rb
+++ spec/models/candidate/project_spec.rb
@@ -1,5 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Candidate::Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:candidate) {Candidate.new(first_name:"abcd",last_name:"efgh",dob:'1992/03/15',gender:'M',marital_status:"married",status:true,languages:"HIBRU",summary:"dont know",phone:"12321442415",interests:"dddd")}
+
+	let(:project) {Candidate::Project.new(title:"log analytics",domain:"Big DATA",end_date:Time.now,position:"intern",organisation:"BMC",information:"adkjjahsdkjh kjashdkj")}
+
+
+	context "model with attributes valid?" do	
+		it "is valid with valid attributes" do
+		    expect(candidate).to be_valid
+		    candidate.save!
+		    candidate.projects << project
+		    expect(candidate.projects.first).to be_valid
+		 end
+	end
+
+	context "title" do
+		it "invalid! field required." do
+			project.title = ""
+			expect(project).to be_invalid
+		end
+
+		it "invalid! character limit 50 exceeded." do
+			project.title = "g"*51
+			expect(project).to be_invalid
+		end
+	end
+
+	context "domain" do
+		it "invalid! character limit 50 exceeded." do
+			project.domain = "g"*51
+			expect(project).to be_invalid
+		end
+	end
+
+	context "end date" do
+		it "invalid! field required." do
+			project.end_date = ""
+			expect(project).to be_invalid
+		end
+	end
+
+	context "position" do
+		it "invalid! field required." do
+			project.position = ""
+			expect(project).to be_invalid
+		end
+
+		it "invalid! character limit 35 exceeded." do
+			project.position = "g"*36
+			expect(project).to be_invalid
+		end
+	end
+
+	context "organisation" do
+		it "invalid! field required." do
+			project.organisation = ""
+			expect(project).to be_invalid
+		end
+
+		it "invalid! character limit 35 exceeded." do
+			project.organisation = "g"*36
+			expect(project).to be_invalid
+		end
+	end
+
+
+	context "information" do
+		it "invalid! field required." do
+			project.information = ""
+			expect(project).to be_invalid
+		end
+
+		it "invalid! character limit 65535 exceeded." do
+			project.information = "g"*65536
+			expect(project).to be_invalid
+		end
+	end
 end
