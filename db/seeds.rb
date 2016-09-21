1fa1f22f79f284c5726ea5ac74f9424ef9ecc633
akash <akash.s@amuratech.com>
2016-09-21 12:45:54 +0530

    added .swo and added users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+def create_users(user_count=20)
+  puts "Generating test users from seed.....(check out file in tmp/test_users.txt)"
+  File.open(Rails.root.join('tmp','test_users.txt'),'w') do |file|
+    user_count.times do |count|
+      email = Faker::Internet.email 
+      password = Faker::Internet.password
+      file.puts "#{count+1}) #{email}|#{password}"
+      user = User.new(email:email,password:password)
+      user.save!
+    end
+  end
+end
+
+create_users
