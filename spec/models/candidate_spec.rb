5f37216a4013030d3f427e70a2aeea908984d51f
jayesh <jayesh@amuratech.com>
2016-09-22 14:44:52 +0530

    fixed candidate validation spec

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Candidate, type: :model do
 
-	let(:candidate) {Candidate.new(first_name:"abcd",last_name:"efgh",dob:'1992/03/15',gender:'M',email:'jayesh@gmail.com',marital_status:"married",status:true,languages:"HIBRU",summary:"dont know",phone:"12321442415",interests:"dddd")}
+	let(:candidate) {Candidate.new(first_name:"abcd",last_name:"efgh",dob:'1992/03/15',gender:'M',marital_status:"married",status:true,languages:"HIBRU",summary:"dont know",phone:"12321442415",interests:"dddd")}
 
 	context "model with attributes valid?" do
 		it "is valid with valid attributes" do
