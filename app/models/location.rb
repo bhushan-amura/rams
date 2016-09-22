310dfa1eeeb2fdf00d8ae354d3879f4c01ada353
bhushan-amura <bhushan.a@amuratech.com>
2016-09-22 17:19:02 +0530

    Fixes #14 Added location specs

--- app/models/location.rb
+++ app/models/location.rb
@@ -11,14 +11,14 @@ class Location < ActiveRecord::Base
   belongs_to :locatable, polymorphic: true
 
   # validations
-  validates :city, presence:true
-  validates :street_name, presence:true
-  validates :street_address, presence:true
-  validates :building_name, presence:true
+  validates :city, presence:true, length: { maximum:255 }
+  validates :street_name, presence:true, length: { maximum:255 }
+  validates :street_address, presence:true, length: { maximum:255 }
+  validates :building_name, presence:true, length: { maximum:255 }
   validates :building_number, presence:true
-  validates :zipcode, presence:true
-  validates :state, presence:true
-  validates :country, presence:true
+  validates :zipcode, presence:true, length: { maximum:255 }
+  validates :state, presence:true, length: { maximum:255 }
+  validates :country, presence:true, length: { maximum:255 }
 
 
 end
