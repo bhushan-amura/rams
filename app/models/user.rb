065707f5cdcd3f8921ff3dc93c2969a7ed23200c
akash <akash.s@amuratech.com>
2016-09-24 07:46:48 +0530

    added info methods to user to get appropriate information

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Associations
   has_one :admin 
   has_one :candidate
   has_one :company 
+
+  # Instance methods
+  def info
+    self.admin||self.candidate||self.company
+  end
+
+
+
 end
