e531cce986b26e3a4b3cf65b130d8f8f3ac6681f
akash <akash.s@amuratech.com>
2016-09-27 12:15:12 +0530

    refactored achievement_path_params helper

--- app/helpers/qualifications_helper.rb
+++ app/helpers/qualifications_helper.rb
@@ -1,5 +1,6 @@
 module QualificationsHelper
 
+  # Return qualification path of candidate or job as string
   def qualification_path(*prefix_or_suffix)
     prefix_or_suffix = prefix_or_suffix[0]
     path = @entity.instance_of?(Candidate) ? 'candidate_qualification' : 'job_qualification'
@@ -7,6 +8,7 @@ module QualificationsHelper
     path+'_path'
   end
 
+  # Return qualification path params which accepts an object
   def qualification_path_params(qualification)
     id = qualification.id
     @entity.instance_of?(Candidate) ? {candidate_id: params[:candidate_id],id: id} : {company_id: params[:company_id],job_id: params[:job_id],id: id}
