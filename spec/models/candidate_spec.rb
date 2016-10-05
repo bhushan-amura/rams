2cfeda75a3016bd8ca7c71f52fe44492adba50b5
bhushan-amura <bhushan.a@amuratech.com>
2016-10-05 15:01:56 +0530

    Added candidate spec

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Candidate, type: :model do
 
-	let(:candidate) {Candidate.new(first_name:"abcd",last_name:"efgh",dob:'1992/03/15',gender:'M',marital_status:"married",status:true,languages:"HIBRU",summary:"dont know",phone:"12321442415",interests:"dddd")}
+	let(:candidate) {Candidate.new(first_name:"abcd", last_name:"efgh", dob:'1992/03/15', gender:'M', marital_status:"married", status:true, languages:"HIBRU", summary:"dont know", phone:"12321442415", interests:"dddd")}
+
+	let(:institute) { Institute.new(university:'sppu', campus:'pict')}
+
+
+	let(:qualification) {Qualification.new(course:"engineering", domain: "comp")}
 
 	context "model with attributes valid?" do
 		it "is valid with valid attributes" do
@@ -66,14 +71,12 @@ RSpec.describe Candidate, type: :model do
 		end
 	end
 
-
 	context "languages" do
 		it "invalid! character limit 65535 exceeded." do
 			candidate.languages = "g"*65536
 			expect(candidate).to_not be_valid
 		end
 	end
-
 
 	context "summary" do
 		it "invalid! character limit 65535 exceeded." do
@@ -82,7 +85,6 @@ RSpec.describe Candidate, type: :model do
 		end
 	end
 
-
 	context "interests" do
 		it "invalid! character limit 65535 exceeded." do
 			candidate.interests = "g"*65536
@@ -90,22 +92,121 @@ RSpec.describe Candidate, type: :model do
 		end
 	end
 
+	describe "when Association" do
+		it "has many achivements" do
+			assc = Candidate.reflect_on_association(:achievements)
+	    expect(assc.macro).to eq :has_many
+		end
 
-	# validates :email, :presence => true, :length => { :maximum => 100 },
-	# 		 :format => EMAIL_REGEX, :confirmation => true,
-	# 		 :uniqueness => true
+		it "has many experiences" do
+			assc = Candidate.reflect_on_association(:experiences)
+	    expect(assc.macro).to eq :has_many
+		end
 
-	# context "email" do
-	# 	it "invalid! field required." do
-	# 		candidate.email = ""
-	# 		expect(candidate).to be_invalid
-	# 	end
+		it "has many links" do
+			assc = Candidate.reflect_on_association(:links)
+	    expect(assc.macro).to eq :has_many
+		end
 
-	# 	it "invalid! character limit 100 exceeded." do
-	# 	 	candidate.email = Faker::Internet.email
-	# 	 	# puts candidate.email
-	# 	 	expect(candidate).to be_valid
-	# 	end
-	# end
+		it "has many projects" do
+			assc = Candidate.reflect_on_association(:projects)
+	    expect(assc.macro).to eq :has_many
+		end
 
+		it "has many references" do
+			assc = Candidate.reflect_on_association(:references)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many test_scores" do
+			assc = Candidate.reflect_on_association(:test_scores)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many course_scores" do
+			assc = Candidate.reflect_on_association(:course_scores)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many tests" do
+			assc = Candidate.reflect_on_association(:tests)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many reviews" do
+			assc = Candidate.reflect_on_association(:reviews)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many skills" do
+			assc = Candidate.reflect_on_association(:skills)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many location" do
+			assc = Candidate.reflect_on_association(:location)
+	    expect(assc.macro).to eq :has_one
+		end
+
+		it "has many skill_assignments" do
+			assc = Candidate.reflect_on_association(:skill_assignments)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many job_opportunities" do
+			assc = Candidate.reflect_on_association(:job_opportunities)
+	    expect(assc.macro).to eq :has_and_belongs_to_many
+		end
+
+		it "has many candidate_qualification_assignments" do
+			assc = Candidate.reflect_on_association(:candidate_qualification_assignments)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many qualification_assignments" do
+			assc = Candidate.reflect_on_association(:qualification_assignments)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many institutes" do
+			assc = Candidate.reflect_on_association(:institutes)
+	    expect(assc.macro).to eq :has_many
+		end
+
+		it "has many user" do
+			assc = Candidate.reflect_on_association(:user)
+	    expect(assc.macro).to eq :belongs_to
+		end
+	end
+
+	describe Candidate, 'methods' do
+	  context "when name_with_initial" do
+	    it "should return full name(first_name + last_name)" do
+				str = "  "+candidate.first_name+" "+candidate.last_name
+	      expect(candidate.name_with_initial).to eql(str)
+	    end
+	  end
+
+		context "when add_institute_with_qualification" do
+		  it "should return candidate_qualification_assignment object" do
+				candidate.save!
+				institute.save!
+				expect(institute).to be_valid
+				qualification.save!
+				expect(qualification).to be_valid
+		    expect(candidate.add_institute_with_qualification(institute, qualification)).not_to eql(nil)
+		  end
+		end
+
+		context "when get_institutes_with_qualifications" do
+		  it "should return institutes_with_qualifications object" do
+				candidate.save!
+				institute.save!
+				expect(institute).to be_valid
+				qualification.save!
+				expect(qualification).to be_valid
+				expect(candidate.get_institutes_with_qualifications).not_to eql(nil)
+		  end
+		end
+	end
 end
