edf4d3f08862beabcc3b0296f0e3e6d7212be37d
bhushan-amura <bhushan.a@amuratech.com>
2016-10-05 15:00:56 +0530

    Added qualification assignment spec

--- spec/models/qualification_assignment_spec.rb
+++ spec/models/qualification_assignment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe QualificationAssignment, type: :model do
-	# add tests over here
+	context "when Association" do
+    it "belongs to qualification" do
+      assc = QualificationAssignment.reflect_on_association(:qualification)
+      expect(assc.macro).to eq :belongs_to
+    end
+    it "belongs to qualifiable" do
+      assc = QualificationAssignment.reflect_on_association(:qualifiable)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
 end
